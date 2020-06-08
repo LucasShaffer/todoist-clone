80d8b48a1be4050f65b95f03fec65feb3d5168ae
Lucas Shaffer <shafferlucas@gmail.com>
2020-06-07 21:45:08 -0700

    add dark mode

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { FaPizzaSlice } from 'react-icons/fa';
 
-export const Header = () => {
+export const Header = ({darkMode, setDarkMode}) => {
+
   return (
     <header className="header" data-testid="header">
       <nav>
@@ -13,7 +14,11 @@ export const Header = () => {
           <li data-testid="quick-add-task-action" className="settings__add">
             +
           </li>
-          <li data-testid="dark-mode-action" className="settings__darkmode">
+          <li
+            data-testid="dark-mode-action"
+            className="settings__darkmode"
+            onClick={() => setDarkMode(!darkMode)}
+          >
             <FaPizzaSlice />
           </li>
         </ul>
