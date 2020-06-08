1557bd4df2af8d25348317081f713647e26d7db3
Lucas Shaffer <shafferlucas@gmail.com>
2020-06-07 21:37:30 -0700

    implment add task functionality

---
+++ src/components/ProjectOverlay.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useProjectsValue } from '../context';
+
+export const ProjectOverlay = ({setProject, showProjectOverlay, setShowProjectOverlay}) => {
+  const { projects } = useProjectsValue();
+
+  return (
+    projects && showProjectOverlay && (
+      <div className="project-overlay" data-testid="project-overlay">
+        <ul className="project-overlay__list">
+          {projects.map(project => (
+            <li
+              key={project.projectId}
+              data-testid="project-overlay-action"
+              onClick={() => {
+                setProject(project.projectId);
+                setShowProjectOverlay(false);
+              }}
+            >
+              {project.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  )
+}
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,6 +4,7 @@ import { useTasks } from '../hooks';
 import { collatedTasks } from '../constants';
 import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
 import { useSelectedProjectValue, useProjectsValue } from '../context';
+import { AddTask } from './AddTask';
 
 export const Tasks = () => {
   const { selectedProject } = useSelectedProjectValue();
@@ -35,6 +36,8 @@ export const Tasks = () => {
           </li>
         ))}
       </ul>
+
+      <AddTask />
     </div>
   )
 }
